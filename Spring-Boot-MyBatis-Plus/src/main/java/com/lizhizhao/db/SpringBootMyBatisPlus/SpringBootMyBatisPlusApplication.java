00abf5d3051a5f2165a8323e7d5da4455ea81fd9
李志钊 <lizhizhaooo@163.com>
2020-08-14 18:35:10 +0800

    [初始化]

---
+++ Spring-Boot-MyBatis-Plus/src/main/java/com/lizhizhao/db/SpringBootMyBatisPlus/SpringBootMyBatisPlusApplication.java
@@ -0,0 +1,13 @@
+package com.lizhizhao.db.SpringBootMyBatisPlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMyBatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMyBatisPlusApplication.class, args);
+    }
+
+}
---
+++ Spring-Boot-MyBatis-Plus/src/test/java/com/lizhizhao/db/SpringBootMyBatisPlus/SpringBootMyBatisPlusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lizhizhao.db.SpringBootMyBatisPlus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMyBatisPlusApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
