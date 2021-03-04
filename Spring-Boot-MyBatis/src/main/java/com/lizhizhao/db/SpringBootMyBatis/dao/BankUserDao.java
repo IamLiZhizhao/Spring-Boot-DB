a316b5865f6290106c6a3f061c82c9c1194e160f
李志钊 <lizhizhaooo@163.com>
2021-03-04 09:56:32 +0800

    feat:DAO增加@Component注解

--- Spring-Boot-MyBatis/src/main/java/com/lizhizhao/db/SpringBootMyBatis/dao/BankUserDao.java
+++ Spring-Boot-MyBatis/src/main/java/com/lizhizhao/db/SpringBootMyBatis/dao/BankUserDao.java
@@ -7,10 +7,12 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+@Component
 @Mapper
 public interface BankUserDao {
 
